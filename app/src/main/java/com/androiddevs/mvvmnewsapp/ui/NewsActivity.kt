9aaad0743fb81db7b59ea35321d88871c35ad0f7
htk007 <hasankaya921@hotmail.com>
2023-02-24 22:55:08 +0300

    add navigation part 1

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -1,7 +1,8 @@
-package com.androiddevs.mvvmnewsapp
+package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.androiddevs.mvvmnewsapp.R
 
 class NewsActivity : AppCompatActivity() {
 
